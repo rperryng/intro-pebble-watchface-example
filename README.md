1caa0302fccfef257e7c0593c7c25bd7cf094422
Ryan Perry-Nguyen <ryanp@pebble.com>
2016-02-12 14:03:21 -0800

    step 1 (display time)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intro Pebble Watchface Example
+
+example code used for the introductory Pebble C workshop at Stanford February 2016
---
+++ src/workshop.c
@@ -0,0 +1,40 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(s_main_window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_time_layer = text_layer_create(GRect(0, 58, bounds.size.w, 50));
+
+  text_layer_set_text(s_time_layer, "00:00");
+
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_time_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
