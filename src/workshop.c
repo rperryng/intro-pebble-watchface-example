312f693b69a01f0cac0af25bfafd4705e02122d9
Ryan Perry-Nguyen <ryanp@pebble.com>
2016-02-12 17:06:45 -0800

    step 2 (tick handler, customize text

--- src/workshop.c
+++ src/workshop.c
@@ -3,12 +3,30 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(s_main_window);
   GRect bounds = layer_get_bounds(window_layer);
 
   s_time_layer = text_layer_create(GRect(0, 58, bounds.size.w, 50));
 
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorJazzberryJam);
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text(s_time_layer, "00:00");
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -25,6 +43,8 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   window_stack_push(s_main_window, true);
 }
